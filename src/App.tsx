b4beca6ab231cdcbd047210ec378027a1e1baa77
Kyle Silver <kyleAsilver@gmail.com>
2022-10-15 21:32:35 -0400

    word entry mechanic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function is_input_valid(value: string): boolean {
+  return value.search("^[a-z]{0,8}$") >= 0;
+}
 
 function App() {
+  const [words, setWords] = useState<string[]>([]);
+  const [input, setInput] = useState("");
+  const listItems = words.map((word, index) => {
+    return <li key={index}>{word}</li>;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>There are {words.length} words in the list</p>
+      <input
+        type="text"
+        value={input}
+        onInput={(e) => {
+          const value = e.currentTarget.value.toLowerCase();
+          if (is_input_valid(value)) {
+            setInput(value);
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setWords([...words, input]);
+            setInput("");
+          }
+        }}
+      />
+      <ul>{listItems}</ul>
     </div>
   );
 }
