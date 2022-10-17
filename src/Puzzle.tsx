64f142d08532fcf8282551f1491f78619160da92
Kyle Silver <kyleAsilver@gmail.com>
2022-10-16 21:39:48 -0400

    different word list

--- src/Puzzle.tsx
+++ src/Puzzle.tsx
@@ -19,7 +19,7 @@ export class Puzzle {
 }
 
 export async function randomPuzzle(): Promise<Puzzle> {
-  const index = Math.floor(Math.random() * 401_430).toFixed(0);
+  const index = Math.floor(Math.random() * 335_844).toFixed(0);
   const response = await window.fetch(`https://raw.githubusercontent.com/kyle-silver/mystery-box-puzzles/main/puzzles/${index}.json`);
   const json = await response.json();
   console.log("retrieved JSON: ", json);
